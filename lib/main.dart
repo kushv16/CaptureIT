bcd975d8e138f5fdf98e2ec37a0f83910e81d87f
Darshil Mehta <darshilm223@gmail.com>
2021-11-28 23:10:48 +0530

    home screen added

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,7 @@ class _MyHomePageState extends State<MyHomePage> {
     Timer(
         Duration(seconds: 3),
         () => Navigator.pushReplacement(
-            context, MaterialPageRoute(builder: (context) => Home())));
+            context, MaterialPageRoute(builder: (context) => HomePage())));
   }
 
   @override
@@ -49,106 +49,170 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class Home extends StatelessWidget {
-  @override
+class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              width: 375,
-              height: 80,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.yellow,
+        appBar: AppBar(
+          title: Text(
+            "Capture IT",
+            style: new TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          titleSpacing: 00.0,
+          centerTitle: true,
+          toolbarHeight: 60.2,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(360),
+          ),
+          elevation: 0.00,
+          backgroundColor: Colors.yellow,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
                   Container(
-                    color: Colors.yellow,
-                    child: new Image.asset('assets/logo.png'),
-                    alignment: Alignment.center,
-                  )
+                    height: 175,
+                    width: 175,
+                    color: Colors.transparent,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.transparent,
+                        border: Border.all(
+                          color: Colors.black,
+                          width: 3,
+                        ),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(20),
+                        ),
+                      ),
+                      child: new Image.asset('assets/barcode.png'),
+                    ),
+                  ),
+                  Container(
+                    height: 175,
+                    width: 175,
+                    color: Colors.transparent,
+                    child: Container(
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: Colors.transparent,
+                        border: Border.all(
+                          color: Colors.black,
+                          width: 3,
+                        ),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(20),
+                        ),
+                      ),
+                      child: new Image.asset('assets/ocr.png'),
+                    ),
+                  ),
                 ],
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
-            ),
-            SizedBox(
-              width: 375,
-              height: 100,
-              child: FlatButton(
-                child: Text(
-                  'Testing',
-                  style: TextStyle(fontSize: 20.0),
-                ),
-                color: Colors.blueAccent,
-                textColor: Colors.white,
-                onPressed: () {},
+              Padding(
+                padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
-            ),
-            SizedBox(
-              width: 375,
-              height: 100,
-              child: FlatButton(
-                child: Text(
-                  'Testing',
-                  style: TextStyle(fontSize: 20.0),
-                ),
-                color: Colors.blueAccent,
-                textColor: Colors.white,
-                onPressed: () {},
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
-            ),
-            SizedBox(
-              width: 375,
-              height: 100,
-              child: FlatButton(
-                child: Text(
-                  'Testing',
-                  style: TextStyle(fontSize: 20.0),
-                ),
-                color: Colors.blueAccent,
-                textColor: Colors.white,
-                onPressed: () {},
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(40, 40, 40, 40),
-            ),
-            Material(
-              color: Colors.deepPurple,
-              borderRadius: BorderRadius.circular(changeButton ? 50 : 8),
-              child: InkWell(
-                onTap: () => moveToHome(context),
-                child: AnimatedContainer(
-                  duration: Duration(seconds: 1),
-                  width: changeButton ? 50 : 150,
-                  height: 50,
-                  alignment: Alignment.center,
-                  child: changeButton
-                      ? Icon(
-                          Icons.done,
-                          color: Colors.white,
-                        )
-                      : Text(
-                          "Login",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 18),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Container(
+                    height: 175,
+                    width: 175,
+                    color: Colors.transparent,
+                    child: Container(
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.black,
+                          width: 3,
                         ),
-                ),
+                        color: Colors.transparent,
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(20),
+                        ),
+                      ),
+                      child: new Image.asset('assets/text.png'),
+                    ),
+                  ),
+                  Container(
+                    height: 175,
+                    width: 175,
+                    color: Colors.transparent,
+                    child: Container(
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: Colors.transparent,
+                        border: Border.all(
+                          color: Colors.black,
+                          width: 3,
+                        ),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(20),
+                        ),
+                      ),
+                      child: new Image.asset('assets/image_to_text.png'),
+                    ),
+                  ),
+                ],
               ),
-            ),
-          ],
+              Padding(
+                padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Container(
+                    height: 175,
+                    width: 175,
+                    color: Colors.transparent,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.transparent,
+                        border: Border.all(
+                          color: Colors.black,
+                          width: 3,
+                        ),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(20),
+                        ),
+                      ),
+                      child: new Image.asset('assets/mic.png'),
+                    ),
+                  ),
+                  Container(
+                    height: 175,
+                    width: 175,
+                    color: Colors.transparent,
+                    child: Container(
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: Colors.transparent,
+                        border: Border.all(
+                          color: Colors.black,
+                          width: 3,
+                        ),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(20),
+                        ),
+                      ),
+                      child: new Image.asset('assets/map.png'),
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+              ),
+            ],
+          ),
         ),
       ),
     );
